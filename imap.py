a855badef4c9dd158f2a940c04e43cbf55ee6171
spaceyassy <junjun.yasyas@gmail.com>
2026-02-09 17:43:17 +0900

    Uncomment line to print each line of body

--- imap.py
+++ imap.py
@@ -133,7 +133,7 @@ class MyEmail:
         nexturl = False
         for line in self.body.splitlines():
             #print(nexturl)
-            #print(line)
+            print(line)
             #下の文字列が見つかってから最初のHTTP かつ src ではない
             if ('↓ クリックでもれなく1ポイントGet!! ↓' in line 
                 or '▼楽天ポイント獲得はこちら▼' in line
